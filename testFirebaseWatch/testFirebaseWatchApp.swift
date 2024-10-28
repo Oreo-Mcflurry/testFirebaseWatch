2bd049f83ab41a979354ce2125447f38eeff4a3a
Musk <yuiop1029@naver.com>
2024-10-28 14:17:59 +0900

    Initial Commit

---
+++ testFirebaseWatch/testFirebaseWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  testFirebaseWatchApp.swift
+//  testFirebaseWatch
+//
+//  Created by Musk on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct testFirebaseWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
